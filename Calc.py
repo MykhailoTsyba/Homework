26dd22a954912f6e7cca99106c8933efe922ee41
MykhailoTsyba <mykhailotsyba@gmail.com>
2024-03-11 21:44:33 +0200

    Work has done

---
+++ Calc.py
@@ -0,0 +1,15 @@
+operation ="+"
+number_1 = 9
+number_2 = 2
+
+if operation == "+":
+    result = number_1 + number_2
+elif operation == "-":
+    result = number_1 + number_2
+elif operation == "*":
+    result = number_1 * number_2
+elif operation == "/":
+    result = number_1 / number_2
+else: result = "eror"
+
+print(result)
