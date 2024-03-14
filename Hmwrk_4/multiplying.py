d4c7fa030571c90b43ab52fe408e8576ab161b7a
MykhailoTsyba <mykhailotsyba@gmail.com>
2024-03-14 20:22:22 +0200

    work has done

---
+++ Hmwrk_4/multiplying.py
@@ -0,0 +1,6 @@
+print("Welcome to strings multipler!")
+text = str(input("Enter the text: "))
+number = int(input("How much strings do you want to see?\nEnter amount: "))
+result = text * number
+
+print(f"Your result is: {result}\n\nThank you for using our multipler\nLet problems and disagreements don\'t make you weather in life\nAll the Best to you\nGood luck and be healthy!")
