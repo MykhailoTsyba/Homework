7b6ab2850c77c979cb34827fe5c25ac6d0fe0a8d
MykhailoTsyba <mykhailotsyba@gmail.com>
2024-03-21 12:51:09 +0200

    Work has done

--- Hmwrk_5(list_and_tuple)/task_1.py
+++ Hmwrk_5(list_and_tuple)/task_1.py
@@ -1,11 +1,2 @@
-text = 'Some men interpret nine memos'
-text_1 = text.lower()
-text_2 = text_1.replace(' ', '')
-list_1 = list()
-list_1.extend(text_2)
-list_reverse = list(reversed(list_1))
-
-if list_1 == list_reverse:
-    print('It\'s a pallindrome')
-else:
-    print('It\'s not a pallindrome')
+actual_str = input("Enter the string: ")
+print(f'Is string {actual_str} palindrome? {actual_str == actual_str[::-1]}')
